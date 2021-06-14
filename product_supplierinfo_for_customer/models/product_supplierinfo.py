1ca7498110b4f0dcdf1a05e1b43c9bb70c8148f7
Ignacio J. Ortega <nacho.ortega@gmail.com>
2021-06-14 15:39:57 +0200

    [UPD] added ir.config_parameter to avoid changing product_code and description

--- product_supplierinfo_for_customer/models/product_supplierinfo.py
+++ product_supplierinfo_for_customer/models/product_supplierinfo.py
@@ -8,11 +8,14 @@ class ProductSupplierInfo(models.Model):
 
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
+        use_customer_code = bool(
+            int(self.env["ir.config_parameter"].sudo().get_param("product_supplierinfo_for_customer"
+                                                                 ".use_customer_code", 1)))
         res = super(ProductSupplierInfo, self).search(
             args, offset=offset, limit=limit, order=order, count=count)
-        if self.env.context.get('customerinfo') and \
-                self._name == 'product.supplierinfo':
-            limit2 = limit-len(res) if limit else limit
+        if self.env.context.get('customerinfo') and use_customer_code and \
+            self._name == 'product.supplierinfo':
+            limit2 = limit - len(res) if limit else limit
             res2 = self.env['product.customerinfo'].search(
                 args, offset=offset, limit=limit2, order=order, count=count)
             res2 = res2.read(
@@ -23,16 +26,19 @@ class ProductSupplierInfo(models.Model):
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
-        if self.env.context.get('customerinfo') and \
-                self._name == 'product.supplierinfo':
+        use_customer_code = bool(
+            int(self.env["ir.config_parameter"].sudo().get_param("product_supplierinfo_for_customer"
+                                                                 ".use_customer_code", 1)))
+        if self.env.context.get('customerinfo') and use_customer_code and \
+            self._name == 'product.supplierinfo':
             has_ids = self.filtered(
                 lambda x: x.id in x._ids and isinstance(x.id, (int,)))
             new_ids = self.filtered(
                 lambda x: x.id in x._ids and not isinstance(x.id, (int,)))
             return super(ProductSupplierInfo, has_ids).read(
                 fields=fields, load=load) + [
-                {f: x[f] for f in x._fields if (
-                    f in fields if fields else True)} for x in new_ids]
+                       {f: x[f] for f in x._fields if (
+                           f in fields if fields else True)} for x in new_ids]
         else:
             return super(ProductSupplierInfo, self).read(
                 fields=fields, load=load)
